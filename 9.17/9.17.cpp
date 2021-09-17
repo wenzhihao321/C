9ec8a09c6774267c3a2144fdc8dcfb7bca4c9341
wenzhihao321 <254742007@qq.com>
2021-09-18 00:07:27 +0800

    阶乘

---
+++ 9.17/9.17.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+//非递归n的阶乘
+int fac1(int k)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= k; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
+//递归n的阶乘
+int fac2(int k)
+{
+	if (k <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return k * fac2(k - 1);
+	}
+}
+int main()
+{
+	int k = 1;
+	int ret = 0;
+	cin >> k;
+	ret = fac1(k);
+	cout << ret<<endl;
+	ret = fac2(k);
+	cout << ret<<endl;
+	return 0;
+}
