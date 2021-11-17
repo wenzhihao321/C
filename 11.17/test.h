e1ae2c286deded44c8164e1382c7afecc9c1d054
wenzhihao321 <254742007@qq.com>
2021-11-17 22:31:09 +0800

    test.h

---
+++ 11.17/test.h
@@ -0,0 +1,14 @@
+#define __TEST_H__
+#ifndef __TEST_H__
+int Add(int x, int y)
+{
+	return x + y;
+}
+
+#endif
+////现代写法，老的编译器可能不能使用
+//#pragma once
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
