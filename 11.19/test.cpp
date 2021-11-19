f37f0ec396213cab07ec2d90c47922b3b717ebf0
wenzhihao321 <254742007@qq.com>
2021-11-19 22:55:33 +0800

    test

---
+++ 11.19/test.cpp
@@ -0,0 +1,26 @@
+#include"SeqList.h"
+void TestSeqList1()
+{
+	SeqList s;
+	SeqListInit(&s);
+	SeqListPushBack(&s, 1);
+	SeqListPushBack(&s, 2);
+	SeqListPushBack(&s, 3);
+	SeqListPushBack(&s, 4);
+	SeqListPushBack(&s, 5);
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPushFront(&s, 0);
+	SeqListPrint(&s);
+	SeqListPopBack(&s);
+	//SeqListPopFront(&s);
+	SeqListPrint(&s);
+	SeqListDestory(&s);
+
+}
+int main()
+{
+	TestSeqList1();
+	return 0;
+}
