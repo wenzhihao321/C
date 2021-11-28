24e70d075b90f74c2e644ac48ec52623a980e5b2
wenzhihao321 <254742007@qq.com>
2021-11-28 12:11:53 +0800

    Queue

---
+++ 11.28/test.cpp
@@ -0,0 +1,23 @@
+#include"Queue.h"
+
+void test()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	QueueDestroy(&q);
+}
+
+int main()
+{
+	test();
+	return 0;
+}
